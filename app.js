de62fafa4df81a11fb909dde64177ae4fdbcaf86
johnnypestana <johnny_gabriel@outlook.com>
2017-01-12 12:22:17 -0200

    Retorna uma página HTML

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const http = require('http')
 
 http.createServer( (req, res) => {
-    res.writeHead(200, { "Content-Type": "text-plain" })
-    res.end("Hello World!")
-}).listen(process.env.PORT)
+
+    res.writeHead(200, { "Content-Type": "text-html" })
+
+    res.end(
+
+        "<html>" +
+            "<head>" +
+                "<title>Hello World!</title>" +
+            "</head>" +
+            "<body>" +
+                "<h1> Hello World! </h1>" +
+                "<p> Lorem ipsum dolor sit amet consectetur </p>" +
+            "</body>" +
+        "</html>"
+
+    )
+
+}).listen( process.env.PORT )
